a2aac0f9843c1ea2b465a2b13510958e5b97b10b
adilet khasanov <akhasa2001@gmail.com>
2024-01-18 12:07:47 -0600

    added all the vpc resources

--- tfClass4/main.tf
+++ tfClass4/main.tf
@@ -33,14 +33,49 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.my-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+
   tags = {
     Name = "${var.vpcname} IGW route table"
   }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "${var.vpcname} NAT"
+  } 
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "nat" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "nat-rt" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.nat.id
 }
