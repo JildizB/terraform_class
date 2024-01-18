29f4abbd0c1595eddaaeef937e9c6e37f833733d
adilet khasanov <akhasa2001@gmail.com>
2024-01-18 10:54:06 -0600

    added vpc resources

---
+++ tfClass4/main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "my-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  region =  var.region
+  tags = {
+    Name = var.vpcname
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.vpcname}public subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "${var.vpcname}private subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "${var.vpcname} internet gateway"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "${var.vpcname} IGW route table"
+  }
+}
--- tfClass4/variables.tf
+++ tfClass4/variables.tf
@@ -3,3 +3,9 @@ variable region {
   default     = "us-east-1"
   description = "default aws region"
 }
+
+variable vpcname {
+  type        = string
+  default     = "custom VPC"
+  description = "description"
+}
